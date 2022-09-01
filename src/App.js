ef74d944c49777570efbcab0a351c9dd5adff3b5
sujansuja <91907122+sujansuja@users.noreply.github.com>
2022-09-01 19:18:03 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import Footer from './components/Footer'
+import  Roadmap from './components/Roadmap'
+import Populatediv from './components/Populatediv';
 import './App.css';
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app'>
+      <header>
+      <Navbar />
       </header>
+      <main>
+        <Main />
+        <h2>Hello</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti suscipit adipisci commodi fugit maxime blanditiis quaerat cum fuga sit optio maiores esse minus id nam ullam recusandae voluptatibus, omnis corrupti.
+    Molestias sunt iusto nemo maxime blanditiis, accusamus delectus quasi iste, praesentium porro cupiditate dolore perferendis non beatae. Aliquam tempore ut facere nesciunt hic ullam temporibus! Qui sint distinctio ab omnis!
+    Assumenda ipsum necessitatibus aperiam porro hic maxime laborum praesentium temporibus numquam illum sequi, rem eum architecto, sit nemo, animi delectus reiciendis! Ea modi rerum debitis expedita! Repellendus inventore tempora velit!
+    Consequuntur ducimus numquam dignissimos nostrum consectetur amet, corporis rerum. Distinctio ducimus voluptas ipsa expedita at id vel a porro soluta modi laudantium, repudiandae minima velit. Quos mollitia eligendi aut maxime.
+    Vel pariatur, distinctio id quam eveniet quod ex dicta temporibus quasi dolorem, quaerat veritatis modi voluptas rem placeat sequi fugit magni! Eaque eligendi quisquam ipsum explicabo voluptates? Qui, laboriosam atque.
+        </p>
+      </main>
+      <Populatediv />
+      <Roadmap/>
+
+      <Footer />
     </div>
   );
 }
