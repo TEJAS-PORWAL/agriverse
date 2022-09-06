1c6667128ff9e6c7c353540ece21aed57c51a286
ThanayKumarYR <99664993+ThanayKumarYR@users.noreply.github.com>
2022-09-06 16:47:49 +0530

    cards

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import './Footer.css';
 
 export default function Footer() {
 
-    const email = 'info@Agriverse.com';
+    const email = 'info@agriverse.com';
     const fblink = 'https://facebook.com/theagriverse';
     const twtlink = 'https://twitter.com/theagriverse';
     const tellink = 'https://telegram.com';
@@ -28,27 +28,30 @@ export default function Footer() {
                 <div className='mid--left'>
                     <form className='mid--form'>
                         <h2 className='form--title'>Send a message</h2>
-                        {/* <label className='form--label' for='name'> Full Name</label> */}
-                        <input type='text' className='form--name' id='name' name='name' placeholder='Full Name' />
-                        {/* <label className='form--label' for='mail' >Email Address</label> */}
-                        <input className='form--email' type='email' id='mail' name='email' placeholder='Email Address' />
-                        <textarea placeholder='Your Message' name='message' className='form--textarea' rows="5"></textarea>
+                        <div className='form--inputs'>
+
+                            <input type='text' className='form--name' id='name' name='name' placeholder='Full Name' />
+                            <input className='form--email' type='email' id='mail' name='email' placeholder='Email Address' />
+                            <input className='form--mobile' type="tel" id='mobile' name='mobile' placeholder='Moblie' pattern="[0-9]{10}" maxlength="10" />
+                            <input type="text" placeholder='Your Message'/>
+                            
+                        </div>
                         <button className='form--btn btn'>
-                        <span className="noselect">SEND MESSAGE</span></button>
+                            <span className="noselect">SEND MESSAGE</span></button>
 
                     </form>
                 </div>
                 <div className='mid--right'>
                     <h2 className='right--title'>Join us in the community</h2>
                     <div className='mid--right--links'>
-                        <a target="_blank" rel='noreferrer' href={fblink}><i className='fab fa-facebook-f'></i>Facebook</a>
-                        <a target="_blank" rel='noreferrer' href={Linkedlink}><i className='fab fa-linkedin-in'></i>Linkedin</a>
-                        <a target="_blank" rel='noreferrer' href={twtlink}><i className='fab fa-twitter'></i>Twitter</a>
-                        <a target="_blank" rel='noreferrer' href={Redditlink}><i className='fab fa-reddit-alien'></i>Reddit</a>
-                        <a target="_blank" rel='noreferrer' href={tellink}><i className='fab fa-telegram'></i>Telegram</a>
-                        <a target="_blank" rel='noreferrer' href={medlink}><i className='fab fa-medium-m'></i>Medium</a>
-                        <a target="_blank" rel='noreferrer' href={gitlink}><i className='fab fa-github-alt'></i>Github</a>
-                        <a target="_blank" rel='noreferrer' href={yTubelink}><i className='fab fa-youtube'></i>Youtube</a>
+                        <a target="_blank" rel='noreferrer' href={fblink}><i className='fab fa-facebook-f'></i></a>
+                        <a target="_blank" rel='noreferrer' href={Linkedlink}><i className='fab fa-linkedin-in'></i></a>
+                        <a target="_blank" rel='noreferrer' href={twtlink}><i className='fab fa-twitter'></i></a>
+                        <a target="_blank" rel='noreferrer' href={Redditlink}><i className='fab fa-reddit-alien'></i></a>
+                        <a target="_blank" rel='noreferrer' href={tellink}><i className='fab fa-telegram'></i></a>
+                        <a target="_blank" rel='noreferrer' href={medlink}><i className='fab fa-medium-m'></i></a>
+                        <a target="_blank" rel='noreferrer' href={gitlink}><i className='fab fa-github-alt'></i></a>
+                        <a target="_blank" rel='noreferrer' href={yTubelink}><i className='fab fa-youtube'></i></a>
                     </div>
                     <div className='mid--right--email'>
                         <a href={`mailto:${email}`}><i className="fa fa-envelope-o"></i> {email}</a>
